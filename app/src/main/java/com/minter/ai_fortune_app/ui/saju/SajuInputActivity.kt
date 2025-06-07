efa5eba968a0e0f1b87a68ad16b9d4b422c90019
Minter <adf5858@naver.com>
2025-06-08 04:24:10 +0900

    Fix: SajuInputActivity.kt 파일명 오류 수정

---
+++ app/src/main/java/com/minter/ai_fortune_app/ui/saju/SajuInputActivity.kt
@@ -0,0 +1,15 @@
+package com.minter.ai_fortune_app.ui.saju
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.minter.ai_fortune_app.R
+
+class SajuInputActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_saju_input)
+
+    }
+}
